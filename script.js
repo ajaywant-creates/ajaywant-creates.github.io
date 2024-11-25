997128601d8c7a555d90562a2b8c49bd16d000f1
ajaywant-creates <datboitho404@gmail.com>
2024-11-25 10:24:16 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Optional: Add interactivity to your portfolio
+console.log("Welcome to My ePortfolio!");
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
